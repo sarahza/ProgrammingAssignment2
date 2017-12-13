1506401e21e4a51756aab68a43101bf5be513373
Sarah ZAOUI <sarah.zaoui1@gmail.com>
2017-12-13 21:28:04 +0100

    added R scritp

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##I will write a pair of functions that cache the inverse 
+##of a matrix, the matrix supplied is always invertible.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##This function creates a special "matrix" 
+##object that can cache its inverse
 
+makecacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinvPerm <- function(invPerm) inv <<- invPerm
+  getinvPerm <- function() inv
+  list(set = set, get = get,
+       setinvPerm = setinvPerm,
+       getinvPerm = getinvPerm)
 }
 
 
-## Write a short comment describing this function
+
+##This function computes the inverse of 
+##the special "matrix" returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinvPerm()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinvPerm(inv)
+  inv
 }
